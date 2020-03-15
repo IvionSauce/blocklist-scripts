ea3ae06597af9b0eed445c074ae96400123b19e7
Ivion <ivion@xs4all.nl>
2020-03-15 03:16:09 +0100

    Second iteration of reducing domains
    Pretty straight-forward, replacing the shell while-read loop with an awk
    implementation. While in structure mostly the same, it is 2 to 3 times
    faster with larger data sets.

--- domains-reduce.sh
+++ domains-reduce.sh
@@ -13,21 +13,28 @@ HOST_BEGIN='^\s*(127\.0\.0\.1|0\.0\.0\.0)'
 # encoded. This environment variable does ensure sort collates the way we want.
 export LC_ALL=C
 
-# Domain names don't contain spaces, so this is a nice 'empty' value.
-checking=" "
-while read -r domain_rev; do
-    if [[ ! $domain_rev =~ $checking ]]; then
-	# Output domain name if it isn't a subdomain of the domain name we're
-	# checking for.
-	echo $domain_rev
-	# Update the pattern we're checking for. We have to escape literal dots
-	# because this is a regexp.
-	checking=^${domain_rev//./\\.}\\..+
-    fi
-done < \
-     <(grep -E $HOST_BEGIN "$@" \
-	   | sed -E s/$HOST_BEGIN'\s+(\S+).*$/\2/' \
-	   | sed -E "/^(${BOGUS})$/d" \
-	   | rev \
-	   | sort -t '.' -k 1,1 -k 2,2 -k 3,3 -k 4,4) \
+
+read -d '' -r awk_script << 'EOA'
+BEGIN {
+    # Domain names don't contain spaces, so this is a nice 'empty' value.
+    checking=" "
+}
+
+$0 !~ checking {
+    # Output domain name if it isn't a subdomain of the domain name we're
+    # checking for.
+    print $0
+    # Update the pattern we're checking for. We have to escape literal dots
+    # because this is a regexp.
+    gsub(/\./, "\\.", $0)
+    checking="^" $0 "\\..+"
+}
+EOA
+
+awk "$awk_script" < \
+    <(grep --text --no-filename -E "$HOST_BEGIN" "$@" \
+	  | sed -E s/"$HOST_BEGIN"'\s+(\S+).*$/\2/' \
+	  | sed -E "/^(${BOGUS})$/d" \
+	  | rev \
+	  | sort -t '.' -k 1,1 -k 2,2 -k 3,3 -k 4,4) \
     | rev
