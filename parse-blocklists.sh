13e1407a330af9ead511aaf3350c4b2a55ff28f9
Ivion <ivion@xs4all.nl>
2020-05-24 22:16:49 +0200

    Streamline parsing of blocklists
    In moving the grep and sed parts into awk it became clear we could merge
    the two shell functions in a single awk program. So we no longer have to
    read the fileset twice, which also allows us to deal with stdin in a
    sane manner.

--- parse-blocklists.sh
+++ parse-blocklists.sh
@@ -7,32 +7,43 @@ IPV6='::|([[:xdigit:]]{1,4}::?){1,7}([[:xdigit:]]{1,4})?'
 
 # How relevant entries start in the hosts file.
 HOST_BEGIN='^\s*(127\.0\.0\.1|0\.0\.0\.0)\s+[^#]'
-# Regexp of bogus addresses, either remnants or errors from a hosts file.
+# Regexp of bogus domains, either errors or remnants of a hosts file.
 BOGUS="${IPV4}|${IPV6}|"'local(host(\.localdomain)?)?'
+# The inverse of how lines in a domain list begin.
+NOT_DOMAIN_BEGIN="^\s*($|#|${IPV4}|${IPV6})"
 
 export LC_ALL=C
 
 # Concatenate all blocklists, making sure the last and first line of subsequent
-# files don't get mushed together. Then normalize by making everything lowercase
-# and replacing carriage returns.
+# files don't get mushed together. Unless we're dealing with no arguments,
+# assume stdin in that case.
+# Then normalize by making everything lowercase and replacing carriage returns.
+normalize() {
+    tr '[:upper:]\r' '[:lower:]\n'
+}
 pre() {
-    for file in $@; do
-	cat -- "$file"
-	echo
-    done | tr '[:upper:]\r' '[:lower:]\n'
+    if [[ $# > 0 ]]; then
+	for file in $@; do
+	    cat -- "$file"
+	    echo
+	done | normalize
+    else
+	normalize
+    fi
 }
 
-strip-domainlists() {
-    pre "$@" \
-	| grep --invert-match -E "^\s*($|#|${IPV4}|${IPV6})" \
-	| awk '{ print $1 }'
+awk-prep() {
+    # To appease awk: replace a backslash with two backslashes.
+    echo "${@//\\/\\\\}"
+}
+# Strip away all the extraneous stuff from the input so that we get a list of
+# just domains, seperated by newlines.
+strip-fluff() {
+    awk -v host_begin=$(awk-prep "$HOST_BEGIN") \
+	-v bogus=$(awk-prep "^(${BOGUS})$") \
+	-v domain_begin_inverse=$(awk-prep "$NOT_DOMAIN_BEGIN") \
+	'$0 ~ host_begin { if ($2 !~ bogus) { print $2 } next }
+	 $0 !~ domain_begin_inverse { print $1 }'
 }
 
-strip-hostsfiles() {
-    pre "$@" \
-	| grep -E "$HOST_BEGIN" \
-	| awk '{ print $2 }' \
-	| sed -E "/^(${BOGUS})$/d"
-}
-
-{ strip-domainlists "$@"; strip-hostsfiles "$@"; }
+pre "$@" | strip-fluff
