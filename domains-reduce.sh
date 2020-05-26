ef445affd63363c043ac8b542d0511f8bd547e59
Ivion <ivion@xs4all.nl>
2020-05-27 00:14:40 +0200

    Further improve `domains-reduce.sh` performance
    Making the awk script responsible for the unique constraint works out to
    be faster than having `sort` do it, about 20% faster. On my machine it
    shaves off another second from the run time of domains-reduce. For a
    little over a million domains it now takes 5 to 6 seconds.
    
    I will be hard pressed to make it any faster, since most of the run time
    is spent on waiting for sort to complete. I've experimented with the
    --parallel and --buffer-size options to sort, but the speed gain is
    small for my data set... I figure they're more significant for larger
    data sets. No, the most significant speed ups happen by reducing the
    amount of keys `sort` has to sort on.
    
    The other option would be writing my own sorting algorithm specifically
    for domains; I've made some naive efforts in making the awk script sort
    domains, but it was much slower than `sort`. I reckon the way to even
    more performance would be handcrafting (for this specific problem) a
    sorting program in some fast, low-level language and see if GNU sort can
    be beat.
    
    Also extensively update and expand comments.

--- domains-reduce.sh
+++ domains-reduce.sh
@@ -1,32 +1,47 @@
 #!/bin/bash
 
-# Forcing the C locale makes rev/sort(/regex?) run _slightly_ faster, but it's
-# near negligible. While it shouldn't cause any issues - since proper domain
-# names are ASCII - there are some hosts files that do erroneously contain
-# non-ASCII characters in domain names. Unicode domain names should be punycode
-# encoded. This environment variable does ensure sort collates the way we want.
+# Forcing the C locale shouldn't cause any issues - since proper domain names
+# are ASCII - but there are some blocklists that do contain non-ASCII characters
+# in domain names. Those domains need to be put through `idn2` before serving as
+# input to this script. It _will choke_ on Unicode (mostly `rev`).
+# This environment variable ensures sort collates the way we want.
 export LC_ALL=C
 
+# This awk script removes duplicated domains and folds subdomains into their
+# parent domain in one fell swoop, thanks to the work done by `rev` and `sort`.
+# Having `sort` perform the kind of sort we want, parent domains before child
+# domains, _without_ using rev is *much* slower. It makes the logic in the awk
+# script simpler as well: we just have to check if a (sub)domain starts with the
+# current (parent) domain we're checking for.
 read -d '' -r awk_script << 'EOA'
 BEGIN {
     # Domain names don't contain spaces, so this is a nice 'empty' value.
-    checking=" "
+    checking = " "
 }
 
-index($0, checking) != 1 {
-    # Output domain name if it isn't a subdomain of the domain name we're
-    # checking for.
+new_domain() {
+    # Output domain name if we haven't seen it before and it isn't a subdomain
+    # of the domain name we're checking for.
     print $0
     # Update the domain we're checking for.
-    checking=$0 "."
+    checking = $0
+}
+
+# Return false if it's the same domain or a subdomain.
+function new_domain() {
+    return !(index($0, checking) == 1 &&
+	     (length() == length(checking) || index($0, checking ".") == 1))
 }
 EOA
 
 # Manually writing out key-indices is for chumps. Also 63 subdomains should be
 # enough for everyone.
 # Limiting this to fewer subdomains, for example 3, can shave a few seconds off
-# the runtime of sort. But I prefer the certainty of a more exhaustive sort.
-auto_keys() {
+# the run time of `sort`. I specifically mention "3" because, with the blocklist
+# data I have, anything more exhaustive doesn't net you any more reductions.
+# But I prefer the certainty of a more exhaustive sort; I cannot predict what
+# kind of data other people have either.
+auto-keys() {
     for i in {1..64}; do
 	printf -- "-k %s,%s " $i $i
     done
@@ -36,4 +51,4 @@ auto_keys() {
 # if the 'www' subdomain is to be blocked that we might as well block the entire
 # domain.
 sed -E 's/^(www\.)+(.+\..+)/\2/' -- "$@" \
-    | rev | sort -u -t '.' $(auto_keys) | awk "$awk_script" | rev
+    | rev | sort -t '.' $(auto-keys) | awk "$awk_script" | rev
